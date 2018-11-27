eee39a8a085d8c3897ec6542433c3a94453316fa
zhaochanghui <2636190619@qq.com>
2018-11-27 22:26:49 +0800

    'go语法1'

---
+++ day01/base1.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a uint32 =99999
+	var b int64 = -9999929999999
+	var c  float32 = -9999.99
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+
+
+
+/*
+go语言的组成部分：
+包声明
+引入包
+函数
+变量
+语句&表达式
+注释
+
+数据类型：
+布尔 var b bool = true
+数字类型 整型int,  浮点型 float32,float64,
+字符串，
+ */
